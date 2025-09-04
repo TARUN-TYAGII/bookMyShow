d45467141b6c9c0c71c344049408216dd3f13f4c
Tarun Tyagi <tarunt@adobe.com>
2025-09-04 15:24:53 +0530

    adding seat

--- src/main/java/com/example/demo/repositories/SeatRepository.java
+++ src/main/java/com/example/demo/repositories/SeatRepository.java
@@ -1,5 +1,25 @@
 package com.example.demo.repositories;
 
-public class SeatRepository {
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.models.Seat;
+
+@Repository
+public class SeatRepository {
+    List<Seat> seats = new ArrayList<>();
+    private AtomicLong idGenerator = new AtomicLong(1);
+    public Seat createSeat(Seat seat) {
+        seat.setId(idGenerator.getAndIncrement());
+        seat.setCreatedAt(new Date());
+        seat.setUpdatedAt(new Date());
+        seats.add(seat);
+        return seat;
+    }
+
+    
 }
--- src/main/java/com/example/demo/services/SeatService.java
+++ src/main/java/com/example/demo/services/SeatService.java
@@ -1,5 +1,23 @@
 package com.example.demo.services;
 
+import java.util.Date;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.models.Seat;
+import com.example.demo.repositories.SeatRepository;
+
+@Service
 public class SeatService {
 
+    private final SeatRepository seatRepository;
+
+    public SeatService(SeatRepository seatRepository){
+        this.seatRepository = seatRepository;
+    }
+
+    public Seat createSeat(Seat seat){
+        return seatRepository.createSeat(seat);
+    }
 }
